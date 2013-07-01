dbb146abf03cf2406045de47a3c795ada55965c3
shireenrao <shireenrao@gmail.com>
2013-07-01 16:14:21 -0400

    Get latest changes from Prod to github

--- src/imagesorter/imagesorter.py
+++ src/imagesorter/imagesorter.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 '''
-Created on Jul 07, 2012
+Created on Feb 7, 2013
 
 @author: shireenrao
-
 NAME
     imagesorter - This is a utility to sort images into directories based on
               year/month and date taken from the exif data of the image
@@ -29,45 +28,86 @@ Usage:
 
     A log file is created in the same location as the one would run the script.
 '''
-import os, sys, errno
+import sys
+import EXIF
+import datetime
+import os.path
+import time
+import os
+import errno
 import shutil
 import getopt
-import glob
 import logging
-import time
-import datetime, calendar
-from PIL import Image
-from PIL.ExifTags import TAGS
+import calendar
+from hachoir_core.error import HachoirError
+from hachoir_core.cmd_line import unicodeFilename
+from hachoir_parser import createParser
+from hachoir_core.tools import makePrintable
+from hachoir_metadata import extractMetadata
+from hachoir_core.i18n import getTerminalCharset
+from PIL import Image #@UnresolvedImport
+from PIL.ExifTags import TAGS #@UnresolvedImport
 
-_version = 0.1
+
+_version = 0.2
 _source = ""
 _target = ""
 
+
 def version():
     """This Prints the version"""
-    print "imagesorter.py: Version " + str(_version)
+    print "sorter.py: Version " + str(_version)
+
 
 def usage():
     """This prints the usage."""
     print __doc__
 
-def get_create_date(exif_data):
-    """Get embedded create date from EXIF data."""
-    retval=None
 
-    for (k,v) in exif_data.items():
-        key = str(k)
-        if "DateTimeOriginal" in key:
-            retval = datetime.datetime.strptime(v,'%Y:%m:%d %H:%M:%S')
+def get_hachoir_create_date(fname):
+    global log
+    retval = None
+    filename, realname = unicodeFilename(fname), fname
+    parser = createParser(filename, realname)
+    if not parser:
+        log.critical( 'Unable to parse file ' + fname)
+        return retval
+    try:
+        metadata = extractMetadata(parser)
+    except HachoirError, err:
+        log.critical( 'Metadata extraction error for ' + fname + ' - '+ unicode(err))
+        metadata = None
+    if not metadata:
+        log.critical( 'Unable to extract metadata for ' + fname )
+        return retval
+
+    metaitems = metadata.getItems('creation_date')
+    if not metaitems:
+        log.critical( 'Unable to extract metaitmes for ' + fname )
+        return retval
+
+    date_str = metaitems.values[0].text
+    if not date_str:
+        log.critical( 'Unable to extract creation date for ' + fname )
+    else:
+        try:
+            retval = datetime.datetime.strptime(str(date_str), '%Y:%m:%d %H:%M:%S')
+        except:
+            try:
+                retval = datetime.datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
+            except:
+                retval = None
 
     return retval
 
-def get_exif_data(fname):
+
+def get_pil_exif_data(fname):
     """Get embedded EXIF data from image file."""
+    global log
     ret = {}
     try:
         img = Image.open(fname)
-        if hasattr( img, '_getexif' ):
+        if hasattr(img, '_getexif'):
             exifinfo = img._getexif()
             if exifinfo != None:
                 for tag, value in exifinfo.items():
@@ -75,18 +115,88 @@ def get_exif_data(fname):
                     ret[decoded] = value
     except IOError:
         log.critical( 'IOERROR ' + fname)
+    except:
+        log.critical( 'ERROR ' + fname)
+        ret = {}
     return ret
+
+
+def get_exif_data(fname):
+    """Get embedded EXIF data from image file."""
+    global log
+    tags = {}
+    try:
+        img = open(fname, 'rb')
+        tags = EXIF.process_file(img)
+        img.close()
+    except IOError:
+        log.critical('IOERROR ' + fname)
+    except:
+        tags = get_pil_exif_data(fname)
+    return tags
+
+
+def get_pil_create_date(exif_data):
+    """Get embedded create date from EXIF data."""
+    retval = None
+
+    for (k, v) in exif_data.items():
+        key = str(k)
+        if "DateTimeOriginal" in key:
+            retval = v.strip() #datetime.datetime.strptime(v,'%Y:%m:%d %H:%M:%S')
+
+    return retval
+
+
+def get_create_date(exif_data):
+    """Get embedded create date from EXIF data."""
+    retval = None
+    try:
+        date_str = exif_data['EXIF DateTimeOriginal'].values
+    except:
+        try:
+            date_str = get_pil_create_date(exif_data)
+        except:
+            date_str = None
+
+    if date_str:
+        try:
+            retval = datetime.datetime.strptime(str(date_str), '%Y:%m:%d %H:%M:%S')
+        except:
+            try:
+                retval = datetime.datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
+            except:
+                retval = None
+
+    return retval
+
+
+def has_thm_file(filename):
+    """For given thm filename, find corresponding avi file. Return true if found """
+    retval = (False, '', '')
+    dir_name = os.path.dirname(filename)
+    file_name = os.path.basename(filename)
+    f_name, f_ext = os.path.splitext(file_name)
+    thm_file1 = os.path.join(dir_name, f_name + '.THM')
+    thm_file2 = os.path.join(dir_name, f_name + '.thm')
+    if not os.path.isfile(thm_file1):
+        if os.path.isfile(thm_file2):
+            retval = (True, f_name+'.thm', thm_file2)
+    else:
+        retval = (True, f_name+'.THM', thm_file1)
+    return retval
+
 
 def main(argv):
     global log
-    formats = ('jpg')
+    formats = ('jpg', 'avi', 'cr2', 'mov', 'mp4')
     log = logging.getLogger()
-    ch  = logging.StreamHandler()
+    ch = logging.StreamHandler()
 
-    localtime   = time.localtime()
-    timeString  = time.strftime("%Y%m%d%H%M%S", localtime)
+    localtime = time.localtime()
+    timeString = time.strftime("%Y%m%d%H%M%S", localtime)
     debug = True
-    log_file = os.path.join(os.getcwd(),'imagesorter.log.' + timeString)
+    log_file = os.path.join(os.getcwd(), 'imagesorter.log.' + timeString)
 
     if os.path.exists(os.path.dirname(log_file)):
         fh = logging.FileHandler(log_file)
@@ -126,13 +236,12 @@ def main(argv):
         elif o in ("-v", "--version"):
             version()
             exit()
-        elif o in ("-s","--source"):
+        elif o in ("-s", "--source"):
             _source = a
         elif o in ("-t", "--target"):
             _target = a
         else:
             assert False, "unhandled option"
-
 
     if not _source:
         _source = os.getcwd()
@@ -154,36 +263,57 @@ def main(argv):
 
     log.info("Processing and sorting " + _source + " to target " + _target)
 
-
     fileList = []
     for root, dirs, files in os.walk(_source):
         for name in files:
-            filename = os.path.join(root,name)
+            filename = os.path.join(root, name)
             if filename.lower().endswith(formats):
                 fileList.append(filename)
 
-    problems_loc = os.path.join(_target,"exif_problems")
+    problems_loc = os.path.join(_target, "exif_problems")
     try:
         os.makedirs(problems_loc)
     except OSError as exc:
         if exc.errno == errno.EEXIST:
             pass
-        else: raise
+        else:
+            raise
     to_be_processed = len(fileList)
-    log.info ("images to be processed -> " + str(to_be_processed))
+    log.info("images to be processed -> " + str(to_be_processed))
 
     processCount = 0
     nocreatedateCount = 0
     copyCount = 0
     skipCount = 0
     exceptionCount = 0
+    avionlycount = 0
+    avithmcount = 0
+    has_avi = False
     for file in fileList:
         processCount = processCount + 1
         filename = os.path.basename(file)
-        #foldername = os.path.dirname(file)
-        exif = {}
-        exif = get_exif_data(file)
-        create_date = get_create_date(exif)
+        orig_path = os.path.dirname(file)
+        has_thm = False
+        is_video_file = False
+        if filename.lower().endswith('avi'):
+            is_video_file = True
+            has_thm, thm_filename, thm_fullpath = has_thm_file(file)
+        if filename.lower().endswith(('mov', 'mp4')):
+            is_video_file = True
+
+        create_date = None
+        if not is_video_file:
+            exif = get_exif_data(file)
+            create_date = get_create_date(exif)
+        else:
+            if is_video_file and has_thm:
+                avithmcount = avithmcount + 1
+                exif = get_exif_data(thm_fullpath)
+                create_date = get_create_date(exif)
+            else:
+                avionlycount = avionlycount + 1
+                create_date = get_hachoir_create_date(file)
+
         if create_date:
             year_str = str(create_date.year)
             month = create_date.month
@@ -192,48 +322,74 @@ def main(argv):
             if month < 10:
                 month_str = '0'+str(month)
             else:
-                month_str= str(month)
+                month_str = str(month)
             if day < 10:
                 day_str = '0'+str(day)
             else:
-                day_str=str(day)
+                day_str = str(day)
 
             folder_name = year_str + "_" + month_str + "_" + day_str
-            path = os.path.join(_target,year_str,month_name,folder_name)
+            destpath = os.path.join(_target, year_str, month_name, folder_name)
             try:
-                os.makedirs(path)
+                os.makedirs(destpath)
             except OSError as exc:
                 if exc.errno == errno.EEXIST:
                     pass
-                else: raise
-            dest_file = os.path.join(path,filename)
+                else:
+                    raise
+            dest_file = os.path.join(destpath, filename)
             try:
                 if not os.path.isfile(dest_file):
-                    shutil.copy2(file, path)
+                    shutil.copy2(file, destpath)
                     copyCount = copyCount + 1
-                    log.info( "(" + str(processCount) + "/" + str(to_be_processed) +") " + filename + " => " + path)
+                    log.info("(" + str(processCount) + "/" + str(to_be_processed) + ") " + filename + " => " + destpath)
                 else:
                     skipCount = skipCount + 1
-                    log.error( "(" + str(processCount) + "/" + str(to_be_processed) +") " + file + " not copied as already exists in destination")
+                    log.error("(" + str(processCount) + "/" + str(to_be_processed) + ") " + file + " not copied as already exists in destination")
             except OSError as exc:
                 exceptionCount = exceptionCount + 1
-                log.critical( "(" + str(processCount) + "/" + str(to_be_processed) +") " + "Skipped " + file + " due to exception!")
+                log.critical("(" + str(processCount) + "/" + str(to_be_processed) + ") " + "Skipped " + file + " due to exception!")
                 pass
+            if has_thm:
+                dest_thmfile = os.path.join(destpath, thm_filename)
+                try:
+                    if not os.path.isfile(dest_thmfile):
+                        shutil.copy2(thm_fullpath, destpath)
+                        #copyCount = copyCount + 1
+                        log.info("(" + str(processCount) + "/" + str(to_be_processed) + ") " + thm_filename + " => " + destpath)
+                    else:
+                        #skipCount = skipCount + 1
+                        orig_thm_file = os.path.join(orig_path, thm_filename)
+                        log.error("(" + str(processCount) + "/" + str(to_be_processed) + ") " + orig_thm_file + " not copied as already exists in destination")
+                except OSError as exc:
+                    #exceptionCount = exceptionCount + 1
+                    log.critical("(" + str(processCount) + "/" + str(to_be_processed) + ") " + "Skipped " + file + " due to exception!")
+                    pass
+
         else:
             nocreatedateCount = nocreatedateCount + 1
-            log.error( "(" + str(processCount) + "/" + str(to_be_processed) +") " + "Skipped " + file + " due to no create date!")
-            new_dest_file = os.path.join(problems_loc,filename)
+            log.error("(" + str(processCount) + "/" + str(to_be_processed) + ") " + "Skipped " + file + " due to no create date!")
+            new_error_dest = os.path.join(problems_loc, orig_path[1:])
+            try:
+                os.makedirs(new_error_dest)
+            except OSError as exc:
+                if exc.errno == errno.EEXIST:
+                    pass
+                else:
+                    raise
+            new_dest_file = os.path.join(new_error_dest, filename)
             try:
                 if not os.path.isfile(new_dest_file):
-                    shutil.copy2(file, problems_loc)
+                    shutil.copy2(file, new_error_dest)
             except:
                 pass
 
-    log.info( "Copy Complete")
-    log.info( "Files not copied because of no create date exif data: " + str(nocreatedateCount))
-    log.info( "Files copied: " + str(copyCount))
-    log.info( "Files skipped as they exist in destination: " + str(skipCount))
-    log.info( "Exceptions while copying: " + str(exceptionCount))
+    log.info("Copy Complete")
+    log.info("Files not sorted because of no create date exif data: " + str(nocreatedateCount))
+    log.info("Files copied: " + str(copyCount))
+    log.info("Files skipped as they exist in destination: " + str(skipCount))
+    log.info("Exceptions while copying: " + str(exceptionCount))
+
 
 if __name__ == '__main__':
     main(sys.argv[1:])
